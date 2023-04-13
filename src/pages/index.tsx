39770144047dcaad2b4ff7501bdad254bf292379
hyeonpearl <hc9064@gmail.com>
2023-04-13 21:09:34 +0900

    TEST: TMDB API 연동 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import Seo from '@/components/Seo';
+import { useEffect, useState } from 'react';
+
+interface Movie {
+  id: number;
+  original_title: string;
+}
 
 export default function Home() {
+  const [movies, setMovies] = useState<Movie>();
+
+  useEffect(() => {
+    fetch(`/api/movies`)
+      .then(res => res.json())
+      .then(data => setMovies(data.results));
+  }, []);
+
   return (
     <>
       <Seo title={'Home'} />
       <main>
-        <h1>Home</h1>
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map(movie => (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
       </main>
     </>
   );
